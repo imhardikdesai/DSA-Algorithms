54ae717ade3d8fbbd6490beec6414abf3f8841d7
amzamani <abuholyangels@gmail.com>
2022-10-03 22:22:58 +0530

    my commit 2 for hacktoberfest

---
+++ C++ Programs/buySellStocks.cpp
@@ -0,0 +1,35 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+     
+      int ans = 0;
+        
+      int lowestSoFarInARR = INT_MAX;
+      int ifSoldToday = 0;  
+      for(int i = 0; i<prices.size();i++){
+          
+          if(lowestSoFarInARR>prices[i]){
+              lowestSoFarInARR = prices[i];
+          }
+          ifSoldToday = prices[i]- lowestSoFarInARR;
+          
+          ans = max(ans,ifSoldToday);
+      }
+        
+        return ans;
+    }
+};
