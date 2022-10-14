e51f20bf4fb686b55277da36963d2e9f42a5f689
Abhishek <jangal&mangal.gov>
2022-10-14 18:36:52 +0530

    Binary Search Using Recursion Method

--- C++ Programs/Binary_Search_Using_Recursion.cpp
+++ C++ Programs/Binary_Search_Using_Recursion.cpp
@@ -1,45 +1,49 @@
-#include<iostream>
+#include <iostream>
+#include <climits>
 using namespace std;
 
-int Binary_Search_Recursion(int a[],int low,int high,int key){
+int Binary_Search_Recursion(int a[], int low, int high, int key)
+{
 
-    int mid=(low+high)/2;
-    if(low>high)
-    return -1;
+    int mid = (low + high) / 2;
+    if (low > high)
+        return -1;
 
-    if(a[mid]==key)
-    return mid;
+    if (a[mid] == key)
+        return mid;
 
-    else if(a[mid]>key)
-        return Binary_Search_Recursion( a, low, mid-1,  key);
+    else if (a[mid] > key)
+        return Binary_Search_Recursion(a, low, mid - 1, key);
 
     else
-    return Binary_Search_Recursion(a, mid+1, high, key);
-
-
+        return Binary_Search_Recursion(a, mid + 1, high, key);
 }
 
-int main(){
+int main()
+{
 
     int size;
-    cin>>size;
+    cin >> size;
     int a[size];
 
-    for(int i=0;i<size;i++){
-        cin>>a[i];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> a[i];
     }
     int key;
-    cin>>key;
+    cin >> key;
 
-    int ans = Binary_Search_Recursion(a,0,size-1,key);
+    int ans = Binary_Search_Recursion(a, 0, size - 1, key);
 
-    if(ans==-1){
-        cout<<"Not Found"<<endl;
+    if (ans == -1)
+    {
+        cout << "Not Found" << endl;
     }
 
-    else{
-        cout<<key<<" Found at "<<ans+1<<endl;
+    else
+    {
+        cout << key << " Found at " << ans + 1 << endl;
     }
 
-        return 0;
+    return 0;
 }
